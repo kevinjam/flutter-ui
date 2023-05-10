b6d62ba2a13badad2e706dcf3be76f7ed8b1f2ec
Parabeac-Cloud <admin@parabeac.com>
2023-05-10 10:58:58 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/flutterui_colors.g.dart
@@ -0,0 +1,24 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class FlutteruiColors {
+
+static const Color 1 = Color(0xff273900);
+
+static const Color 2 = Color(0xff11111f);
+
+static const Color sec = Color(0xff191b1d);
+
+static const Color 12 = Color(0xff84bd00);
+
+static const Color 13 = Color(0xff0fce16);
+
+static const Color 22 = Color(0xff687282);
+
+static const Color gray2 = Color(0xffada4a5);
+}
